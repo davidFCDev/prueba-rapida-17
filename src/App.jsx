8a0a3b0f86d402bf3dc01f215e22c3d0cc8123c5
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-04 21:57:07 +0200

    control errores/eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,70 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
   const movies = responseMovies.Search;
+
+  const mappedMovies = movies.map((movie) => {
+    return {
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster,
+    };
+  });
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Buscando", search);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+  };
 
   return (
     <div>
       <header className="page">
         <h1>Buscador de peliculas</h1>
-        <form>
-          <input type="text" placeholder="Busca una pelicula" />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            name="query"
+            value={search}
+            type="text"
+            placeholder="Busca una pelicula"
+          />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={movies} />
+        <Movies movies={mappedMovies} />
       </main>
     </div>
   );
